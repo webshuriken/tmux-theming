e7adc4f2af4eefc5af6f45e26f1fe8cb9e8ab630
webshuriken <carlos.alford@gmail.com>
2023-12-30 13:09:21 +0000

    component: create/Clock basic structure with controlled components ready

---
+++ app/create/_components/Clock.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+
+export default function Clock({defaultClockFormat = 24}) {
+  const [clockFormat, setClockFormat] = useState(defaultClockFormat);
+  const [clockColour, setClockColour] = useState("#ff00ff");
+
+  function handleColourChange(e) {
+    setClockColour(e.target.value);
+  }
+
+  // TODO: Add a function to handle the radio button change event
+  // Function should update a prop passed from the parent component
+  function handleRadioChange(e) {
+    console.log("Radio Event: ", e.target.value);
+  }
+
+  return (
+    <section>
+      <h3>Clock (displayed inside a pane)</h3>
+      <ul>
+        <li>
+          Text colour: <input type="color" value={clockColour} onChange={handleColourChange} />
+        </li>
+        <li>
+          Clock format:
+          <p>
+            <label>
+              <input 
+                type="radio" 
+                name="clockFormat" 
+                readOnly 
+                value="12" 
+                defaultChecked={clockFormat === 12 ? true : false}
+                onChange={handleRadioChange}
+              />12 hour
+            </label>
+            <label>
+              <input 
+                type="radio" 
+                name="clockFormat" 
+                readOnly 
+                value="24" 
+                defaultChecked={clockFormat === 24 ? true : false} 
+                onChange={handleRadioChange}
+              />24 hour
+            </label>
+          </p>
+        </li>
+      </ul>
+    </section>
+  )
+}
